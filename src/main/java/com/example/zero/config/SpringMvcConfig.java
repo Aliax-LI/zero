a4d26c3165c41c710102e7ec1b472898032674a1
AdminLiXin <1506875977@qq.com>
2020-10-09 17:57:24 +0800

    前端页面整合

--- src/main/java/com/example/zero/config/SpringMvcConfig.java
+++ src/main/java/com/example/zero/config/SpringMvcConfig.java
@@ -1,9 +1,16 @@
 package com.example.zero.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+    }
 }
